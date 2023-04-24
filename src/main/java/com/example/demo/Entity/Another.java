0c26bba9b8091232ebb19a9d6c4da83f7d33f7d5
Adham Zayed <adhamzayed190@gmail.com>
2023-04-24 02:56:08 +0200

    New Entity Added to project

---
+++ src/main/java/com/example/demo/Entity/Another.java
@@ -0,0 +1,37 @@
+package com.example.demo.Entity;
+
+public class Another {
+private long id;
+private String name;
+private String age;
+
+public Another() {
+	super();
+}
+public Another(long id, String name, String age) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.age = age;
+}
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAge() {
+	return age;
+}
+public void setAge(String age) {
+	this.age = age;
+}
+
+
+}
